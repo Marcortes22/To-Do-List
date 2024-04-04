ffb11b8bd1c478f5712a5ce06da2c1a2b4ec34f0
Marco Cortes <104529129+marcortes20@users.noreply.github.com>
2024-04-04 11:16:26 -0600

    remove unuse imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,31 @@
-import { useState, useEffect } from "react"
-import ToDoForm from "./components/ToDoForm"
-import ToDoHeader from "./components/ToDoHeader"
+import { useState, useEffect } from "react";
+import ToDoForm from "./components/ToDoForm";
+import ToDoHeader from "./components/ToDoHeader";
 import ToDoList from "./components/ToDoList";
-import './styles/index.css'
-import ToDoItem, { type TodoItem } from "./components/ToDoItem"
-
-
+import "./styles/index.css";
+import { type TodoItem } from "./components/ToDoItem";
 
 function App() {
-
   // const dafautlItems: TodoItem[] = [];
 
-  const localStorageKey = 'myTodos';
-
+  const localStorageKey = "myTodos";
 
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
 
-
-
   const saveOnLocalStorage = (list: TodoItem[]) => {
-
     const todos = JSON.stringify(list);
 
-    localStorage.setItem(`${localStorageKey}`, (todos));
-
-
+    localStorage.setItem(`${localStorageKey}`, todos);
   };
 
-
-
   useEffect(() => {
-
     const jsonStringRetrieve = localStorage.getItem(localStorageKey);
 
     // Verificar si jsonStringRecuperado no es null antes de usarlo
     if (jsonStringRetrieve !== null) {
       // Analizar la cadena JSON para obtener el arreglo de objetos
       const arrayRetrieve: TodoItem[] = JSON.parse(jsonStringRetrieve);
-      console.log(arrayRetrieve)
+      console.log(arrayRetrieve);
 
       setTodoList(arrayRetrieve);
     }
@@ -46,12 +34,10 @@ function App() {
   // Resto de tu componente...
 
   const addTodo = (newTodo: TodoItem) => {
+    const newTodoList: TodoItem[] = [...todoList, newTodo]; // creé una nueva lista porque a la hora de guardar en el local storage se hace primero que el set del hook y no se actualiza la informacion.
+    setTodoList(newTodoList);
 
-    const newTodoList: TodoItem[] = [...todoList, newTodo] // creé una nueva lista porque a la hora de guardar en el local storage se hace primero que el set del hook y no se actualiza la informacion.
-    setTodoList(newTodoList)
-
-    saveOnLocalStorage(newTodoList)
-
+    saveOnLocalStorage(newTodoList);
   };
 
   const deleteTodo = (index: number) => {
@@ -62,26 +48,19 @@ function App() {
     localStorage.clear();
 
     saveOnLocalStorage(newList);
-
-
-  }
-
+  };
 
   return (
     <>
-
       <section className="flex flex-col  gap-y-10">
         <ToDoHeader Title={"My To-dos"}></ToDoHeader>
 
         <ToDoForm addTodo={addTodo} BtnText={"Save"}></ToDoForm>
 
-        <ToDoList items={todoList} deleteTodo={deleteTodo}>
-        </ToDoList>
+        <ToDoList items={todoList} deleteTodo={deleteTodo}></ToDoList>
       </section>
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
